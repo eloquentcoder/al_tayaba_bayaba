6048cca28bd69cc17e84b212d4fb626c0318ddca
Mfonn <eloquentintech@gmail.com>
2024-12-11 10:46:12 +0100

    subscription_user

--- resources/views/livewire/admin/subscription-request.blade.php
+++ resources/views/livewire/admin/subscription-request.blade.php
@@ -27,25 +27,28 @@
                 </thead>
                 <tbody class="divide-y divide-white bg-white text-black">
                     @foreach ($subscriptions as $key => $subscription)
-                        <tr>
-                            <td class="py-3 px-4">{{ $key + 1 }}</td>
-                            <td class="py-3 px-4 text-black">
-                                {{ $subscription->user->first_name . ' ' . $subscription->user->last_name }}</td>
-                            <td class="py-3 px-4 text-black">{{ $subscription->user->email }}</td>
-                            <td class="py-3 px-4 text-black">{{ $subscription->amount }}</td>
-                            <td class="py-3 px-4 text-black">{{ $subscription->plan->title }}</td>
-                            <td class="py-3 px-4 text-black">{{ $subscription->status }}</td>
-                            <td class="py-3 px-4 text-black">
-                                <svg wire:click="selectSubscription({{ $subscription }})" id="opensubscriptionModalButton"
-                                    xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-                                    stroke-width="1.5" stroke="currentColor" class="size-4 cursor-pointer">
-                                    <path stroke-linecap="round" stroke-linejoin="round"
-                                        d="M2.036 12.322a1.012 1.012 0 0 1 0-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178Z" />
-                                    <path stroke-linecap="round" stroke-linejoin="round"
-                                        d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
-                                </svg>
-                            </td>
-                        </tr>
+                        @if ($subscription->user)
+                            <tr>
+                                <td class="py-3 px-4">{{ $key + 1 }}</td>
+                                <td class="py-3 px-4 text-black">
+                                    {{ $subscription->user->first_name . ' ' . $subscription->user->last_name }}</td>
+                                <td class="py-3 px-4 text-black">{{ $subscription->user->email }}</td>
+                                <td class="py-3 px-4 text-black">{{ $subscription->amount }}</td>
+                                <td class="py-3 px-4 text-black">{{ $subscription->plan->title }}</td>
+                                <td class="py-3 px-4 text-black">{{ $subscription->status }}</td>
+                                <td class="py-3 px-4 text-black">
+                                    <svg wire:click="selectSubscription({{ $subscription }})"
+                                        id="opensubscriptionModalButton" xmlns="http://www.w3.org/2000/svg"
+                                        fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
+                                        class="size-4 cursor-pointer">
+                                        <path stroke-linecap="round" stroke-linejoin="round"
+                                            d="M2.036 12.322a1.012 1.012 0 0 1 0-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178Z" />
+                                        <path stroke-linecap="round" stroke-linejoin="round"
+                                            d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
+                                    </svg>
+                                </td>
+                            </tr>
+                        @endif
                     @endforeach
                 </tbody>
                 {{ $subscriptions->links('pagination::tailwind') }}
@@ -74,7 +77,9 @@
             <div class="my-4 space-y-1">
                 <div class="text-lg text-center space-x-2 text-[#0f277e]">
                     <span>Amount:</span>
-                    <span id="confirmationAmount">{{ $selectedSubscription ? $selectedSubscription->amount : '0.00' }}</span> USD
+                    <span
+                        id="confirmationAmount">{{ $selectedSubscription ? $selectedSubscription->amount : '0.00' }}</span>
+                    USD
                 </div>
             </div>
             <div class="flex gap-2">
